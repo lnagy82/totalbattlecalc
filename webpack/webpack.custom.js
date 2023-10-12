f1de56a9930de1bc1bb46f2c09d5b538724f5ef5
Nagy László <nagy.laszlo@eszfk.hu>
2023-10-12 19:54:46 +0200

    build again

--- webpack/webpack.custom.js
+++ webpack/webpack.custom.js
@@ -123,6 +123,7 @@ module.exports = async (config, options, targetOptions) => {
       output: {
         groupBy: [
           { pattern: './src/main/webapp/i18n/en/*.json', fileName: './i18n/en.json' },
+          { pattern: './src/main/webapp/data/unit.json', fileName: './data/unit.json' },
           // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
         ],
       },
